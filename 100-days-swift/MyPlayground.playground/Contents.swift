c9f10e14a6db25e70c55c8933892ba73c121b921
cesar <cesar.almeida@gsw.com.br>
2020-05-28 02:03:19 -0300

    first commit

---
+++ 100-days-swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Hi!"
+
+var age = 38
+
+var population = 8_000_000
+
+var str1 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141592654
+
+var awesome = true
