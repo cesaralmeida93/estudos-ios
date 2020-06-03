8e9be81d0efe14512df57d963177725ac0980fda
cesar <cesar.almeida@gsw.com.br>
2020-06-03 13:19:56 -0300

    finished day 3(operators and conditions) of 100 days with swift

--- 100-days-swift/MyPlayground.playground/Contents.swift
+++ 100-days-swift/MyPlayground.playground/Contents.swift
@@ -118,4 +118,83 @@ enum Planet: Int {
     case mars
 }
 
+
 let earth = Planet(rawValue: 3)
+
+
+//Day3
+let firstScore = 12
+let secondScore = 4
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+let product = firstScore * secondScore
+let divides = firstScore / secondScore
+let remainder = 13 % secondScore
+
+let meaningOfLife = 42
+let doubleManing = 42 + 42
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beauteles = firstHalf + secondHalf
+
+var scor = 95
+scor -= 5
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+firstScore == secondScore
+firstScore != secondScore
+firstScore < secondScore
+firstScore >= secondScore
+"Taylor" <= "Swift"
+
+score = 9001
+if score > 9000 {
+    print("It's over 9000!")
+} else if score == 9000 {
+    print("It's exactly 9000!")
+} else {
+    print("It's not over 9000!")
+}
+
+let age1 = 12
+let age2 = 21
+if age1 > 18 && age2 > 18 {
+    print("both are over 18")
+}
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
+
+let firstCard = 11
+let secondCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+let weather = "sunny"
+switch weather {
+case "rain":
+    print("Bring an Umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+    fallthrough
+default:
+    print("enjoy your day")
+}
+
+score = 85
+switch score {
+case 0..<50:
+    print("You failed badly")
+case 50...85:
+    print("You did OK")
+default:
+    print("You did great")
+}
+let nameses = ["Piper", "Alex", "Suzanne", "Gloria"]
+print(nameses[0])
+print(nameses[1...3])
+print(nameses[0...])
