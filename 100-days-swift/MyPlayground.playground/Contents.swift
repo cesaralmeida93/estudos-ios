b546b667b9720a765aa81710262049847fb1f491
cesar <cesar.almeida@gsw.com.br>
2020-06-03 10:54:17 -0300

    finished day 2 of 100 days with swift

--- 100-days-swift/MyPlayground.playground/Contents.swift
+++ 100-days-swift/MyPlayground.playground/Contents.swift
@@ -33,3 +33,89 @@ let ironMaidenRocks: Bool = true
 
 
 //Day 2
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Star"
+let cezao = "cezao pow pow pow"
+var beatles: [String] = [john, paul, george, ringo]
+beatles[0]
+beatles.append(cezao)
+beatles
+    
+let colors = Set(["red", "green", "blue"])
+var colors2 = Set(["red", "green", "blue", "red", "blue"])
+colors2.insert("yellow")
+colors2
+
+
+var names = (first: "César", last: "Almeida")
+
+names.first = "Pedro"
+
+var heights = [String: Any]()
+heights = [
+    "César Almeida": 1.80,
+    "Pedro Predo": true
+]
+
+heights["Pedro Predo"]
+
+let favoriteIceCream = [
+    "Paul": "Chocolate",
+    "Sophie": "Vanilla"
+]
+
+favoriteIceCream["Charlotte", default: "Unknown"]
+
+var teams = [String: String]()
+teams["Paul"] = "Red"
+
+var results = [Int]()
+results.append(0)
+
+var words = Set<String>()
+var numbers = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+var resultsis = Array<Int>()
+
+var testeArray = [String]()
+testeArray.append("Teste")
+
+var testeDic = [String: Int]()
+
+testeDic["Cesar"] = 27
+
+var testeSet = Set<String>()
+testeSet.insert("Teste")
+testeSet.insert("Test2")
+testeSet.insert("Test3")
+testeSet.insert("Test4")
+testeSet
+
+enum Results {
+    case sucess
+    case failure
+}
+
+let results2 = Results.sucess
+
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "programming")
+
+enum Planet: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet(rawValue: 3)
