d064c9eb79b40389357cc199bb421137420a2d5b
cesar <cesar.almeida@gsw.com.br>
2020-06-10 11:13:57 -0300

    finished day 9 of 100 days of swift

--- 100-days-swift/MyPlayground.playground/Contents.swift
+++ 100-days-swift/MyPlayground.playground/Contents.swift
@@ -204,70 +204,70 @@ import UIKit
 //print(nameses[0...])
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Day 4
-let count = 1...10
-for number in count {
-    print("The number is \(number)")
-}
-let albums = ["Red", "1989", "Reputation"]
-for album in albums {
-    print("\(album) is on Apple Music")
-}
-for _ in 1...5 {
-    print("pow")
-}
-
-
-var number = 1
-while number <= 20 {
-    print(number)
-    number += 1
-}
-print("Ready or not here I come!")
-
-
-var numb = 1
-repeat {
-    print(numb)
-    numb += 1
-} while numb <= 20
-print("Ready or not, here I come!")
-while false {
-    print("this is false")
-}
-repeat {
-    print("this is false")
-} while false
-
-
-var countDown = 10
-while countDown >= 0 {
-    print(countDown)
-    if(countDown == 4) {
-        print("I'm bored. Let's go now!")
-        break
-    }
-    countDown -= 1
-}
-print("blastOff")
-
-
-for i in 1...10 {
-    if i % 2 == 1 {
-        continue
-    }
-    print(i)
-}
-
-
-var counter = 0
-while true {
-    print(" ")
-    counter += 1
-
-    if counter == 273 {
-        break
-    }
-}
+//let count = 1...10
+//for number in count {
+//    print("The number is \(number)")
+//}
+//let albums = ["Red", "1989", "Reputation"]
+//for album in albums {
+//    print("\(album) is on Apple Music")
+//}
+//for _ in 1...5 {
+//    print("pow")
+//}
+//
+//
+//var number = 1
+//while number <= 20 {
+//    print(number)
+//    number += 1
+//}
+//print("Ready or not here I come!")
+//
+//
+//var numb = 1
+//repeat {
+//    print(numb)
+//    numb += 1
+//} while numb <= 20
+//print("Ready or not, here I come!")
+//while false {
+//    print("this is false")
+//}
+//repeat {
+//    print("this is false")
+//} while false
+//
+//
+//var countDown = 10
+//while countDown >= 0 {
+//    print(countDown)
+//    if(countDown == 4) {
+//        print("I'm bored. Let's go now!")
+//        break
+//    }
+//    countDown -= 1
+//}
+//print("blastOff")
+//
+//
+//for i in 1...10 {
+//    if i % 2 == 1 {
+//        continue
+//    }
+//    print(i)
+//}
+//
+//
+//var counter = 0
+//while true {
+//    print(" ")
+//    counter += 1
+//
+//    if counter == 273 {
+//        break
+//    }
+//}
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Day 5
 //func printHelp() {
@@ -433,3 +433,47 @@ while true {
 //toys.remove(at: 0)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Day 9
+//struct User {
+//    var name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//
+//struct FamilyTree {
+//    init() {
+//        print("Creating family tree")
+//    }
+//}
+//struct Person {
+//    var name: String
+//    lazy var famiyTree = FamilyTree()
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//var ed = Person(name: "Ed")
+//ed.famiyTree
+//
+//struct Student {
+//    static var classSize = 0
+//    var name: String
+//    init(name: String) {
+//        self.name = name
+//        Student.classSize += 1
+//    }
+//}
+//var student1 = Student(name: "powpowpow")
+//print(Student.classSize)
+//
+//struct Person2 {
+//    private var id: String
+//    init(id: String) {
+//        self.id = id
+//    }
+//    func identify() -> String {
+//        return "My social security number is \(id)"
+//    }
+//}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Day 10
