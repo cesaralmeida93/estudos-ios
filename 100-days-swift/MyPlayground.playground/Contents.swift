fbf0cfada4be4cf04352431be67e1344c0657d9b
cesar <cesar.almeida@gsw.com.br>
2020-06-11 00:18:33 -0300

    finished day 10 of 100 days of swift

--- 100-days-swift/MyPlayground.playground/Contents.swift
+++ 100-days-swift/MyPlayground.playground/Contents.swift
@@ -477,3 +477,71 @@ import UIKit
 //}
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Day 10
+//class Dog {
+//    var name: String
+//    var breed: String
+//
+//    init(name: String, breed: String) {
+//        self.name = name
+//        self.breed = breed
+//    }
+//}
+//
+//class Poodle: Dog {
+//    init(name: String) {
+//        super.init(name: name, breed: "Poodle")
+//    }
+//}
+//
+//class Dog {
+//    func makeNoise() {
+//        print("Woof!")
+//    }
+//}
+//class Poodle: Dog {
+//    override func makeNoise() {
+//        print("Yip!")
+//    }
+//}
+//let poppy = Poodle()
+//poppy.makeNoise()
+//
+//final class Dog{
+//    var name: String
+//    var breed: String
+//    init(name: String, breed: String) {
+//        self.name = name
+//        self.breed = breed
+//    }
+//}
+//
+//class Singer {
+//    var name = "Taylor Swift"
+//}
+//var singer = Singer()
+//print(singer.name)
+//var singerCopy = singer
+//singerCopy.name = "Justin Bieber"
+//print(singer.name)
+//print(singerCopy.name)
+
+class Person {
+    var name = "John Doe"
+    
+    init() {
+        print("\(name) is alive!")
+    }
+    
+    func printGreeting() {
+        print("Hello!, i'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more!")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
