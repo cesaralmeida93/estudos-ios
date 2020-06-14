853a818dfe2d61b302236865defceae26cd4e0a1
cesar <cesar.almeida@gsw.com.br>
2020-06-14 14:07:37 -0300

    finished lecture 2(swiftUI) of cs193p course

---
+++ Memorize/Memorize/EmojiMeroryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMeroryGame.swift
+//  Memorize
+//
+//  Created by GSW Mobile Team on 14/06/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCard: emojis.count) { (pairIndex) -> String in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by GSW Mobile Team on 14/06/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCard: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCard {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMathced: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
