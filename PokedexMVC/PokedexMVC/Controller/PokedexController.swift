8a7f893f3a909c3f0b9c3da4a64f05881fce1931
cesar <cesar.almeida@gsw.com.br>
2020-06-19 01:47:49 -0300

    finished init components of pokedex

---
+++ PokedexMVC/PokedexMVC/Controller/PokedexController.swift
@@ -0,0 +1,74 @@
+//
+//  PokedexController.swift
+//  PokedexMVC
+//
+//  Created by GSW Mobile Team on 18/06/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PokedexCell"
+
+class PokedexController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViewComponents()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showSearchBar() {
+        print(124)
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        collectionView.backgroundColor = .white
+        
+        navigationController?.navigationBar.barTintColor = .mainPink()
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.isTranslucent = false
+        
+        navigationItem.title = "Pokedex"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        navigationItem.rightBarButtonItem?.tintColor = .black
+        
+        collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+extension PokedexController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokedexCell
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension PokedexController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 32, left: 8, bottom: 8, right: 8)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (view.frame.width - 36) / 3
+        return CGSize(width: width, height: width)
+        
+    }
+    
+}
---
+++ PokedexMVC/PokedexMVC/View/PokedexCell.swift
@@ -0,0 +1,65 @@
+//
+//  PokedexCell.swift
+//  PokedexMVC
+//
+//  Created by GSW Mobile Team on 19/06/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+class PokedexCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    let imageView: UIImageView = {
+       let iv = UIImageView()
+        iv.backgroundColor = .groupTableViewBackground
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    lazy var nameContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .mainPink()
+        
+        view.addSubview(nameLabel)
+        nameLabel.center(inView: view)
+        
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Bulbassaur"
+        return label
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureComponents()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureComponents() {
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: self.frame.height - 32)
+        
+        addSubview(nameContainerView)
+        nameContainerView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 32)
+    }
+}
