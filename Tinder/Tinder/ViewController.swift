b0baeac442d3c859432e6f51376d6844c50fe325
cesar <cesar.almeida@gsw.com.br>
2020-07-13 11:23:25 -0300

    added archives

---
+++ Tinder/Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by GSW Mobile Team on 08/07/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
