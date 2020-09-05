1f6f8863703cb019e5ef97e3b6a636c91c0c92b1
César Augusto <cesar.almeida@gsw.com.br>
2020-09-04 23:40:10 -0300

    finished storyboard basics

---
+++ storyboards-getting-started/Ratings/Ratings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by GSW Mobile Team on 04/09/20.
+//  Copyright © 2020 GSW Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
